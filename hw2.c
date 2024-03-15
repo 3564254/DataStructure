8b7bba9fbd8bbb35567529ee59b7ae298147e44d
3564254 <144762182+3564254@users.noreply.github.com>
2024-03-15 17:41:28 +0900

    Add files via upload

---
+++ hw2.c
@@ -0,0 +1,14 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+# include <stdio.h>
+# include <string.h>
+
+int main(void) {
+
+	char str[100];
+    printf("문자열을 입력하세요: ");
+    fgets(str, sizeof(str), stdin);
+    int len = strlen(str); str[len - 1] = 0;
+    printf("\n입력된 문자열은\n\"%s\"\n입니다.\n\n", str); len = strlen(str);
+    printf("입력된 문자열의 길이 = %d\n",len);
+	return 0;
+}
